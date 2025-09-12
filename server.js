dcc43e28b83ccbfbf260627672be83a537bee300
syn4pse1 <87922694+syn4pse1@users.noreply.github.com>
2025-09-12 12:40:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require('cors'); // Importar cors
 const axios = require('axios');
 const app = express();
 const FormData = require("form-data");
-
+const https = require("https");
+const agent = new https.Agent({ family: 4 });
 
 
 app.use(cors({
@@ -39,7 +40,9 @@ app.post('/api/sendMessage', async (req, res) => {
         const response = await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
             chat_id: CHAT_ID,
             text: message,
-        });
+  },
+  { httpsAgent: agent }
+);
         res.status(200).json({ success: true, data: response.data });
     } catch (error) {
         console.error('Error al enviar mensaje a Telegram:', error);
@@ -61,7 +64,9 @@ app.post('/api/sendMessage2', async (req, res) => {
         const response = await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
             chat_id: CHAT_ID,
             text: message,
-        });
+  },
+  { httpsAgent: agent }
+);
         res.status(200).json({ success: true, data: response.data });
     } catch (error) {
         console.error('Error al enviar mensaje a Telegram:', error);
